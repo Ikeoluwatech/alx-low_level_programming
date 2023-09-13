8091633723461d0355e9cba77b486aa049df4fe9
Afeez <iamopeyemi62@gmail.com>
2023-09-13 04:20:54 -0700

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,22 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 		}
+		printf("\n");
 	}
-	printf("\n");
-
+	else if (n < 0)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+	}
 }

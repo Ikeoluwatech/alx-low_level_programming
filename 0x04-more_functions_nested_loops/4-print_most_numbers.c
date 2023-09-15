602c8c4262d5c9f6994725159b107ab769e4d982
Afeez <iamopeyemi62@gmail.com>
2023-09-15 03:14:55 -0700

    loop

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,19 @@
-#include "main"
+#include "main.h"
 /**
  * print_most_numbers - most number
- * @i: parameter to be checked
  * Return: 0
  */
 
 void print_most_numbers(void)
 {
 	int i;
-	for (i = 0 && i , 10 i++)
+
+	for (i = 0 && i < 10; i++;)
 	{
-		if (i != 2 && i != 4 )
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 	}
-	{
-		putchar(i + '0');
-	}
-	putchar("\n");
+	_putchar('\n');
 }
